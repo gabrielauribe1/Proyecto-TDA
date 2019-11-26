b9ffd395cec9659c0698dca4e4442a416b12e0c7
Halabito8 <halabito8@gmail.com>
2019-11-26 09:11:38 -0600

    Se anadio proyecto.c

---
+++ proyecto.c
@@ -0,0 +1,131 @@
+// Inclucion de bibliotecas
+#include "maquina_de_estados.h"
+
+int main(void){
+    system("clear");
+    States MachineState=LOG_IN;
+    //maquina de estados con sus respectivas funciones
+    StateMachine Machine[] = {
+        {LOG_IN, LogIn},
+        {MENU_PRINCIPAL, MenuPrincipal},
+        {VER_PROYECTOS, VerProyectos},
+        {COMMIT, Commit},
+        {REVERT, Revert},
+        {PULL, Pull},
+        {STATUS, Status},
+        {CREAR_PROYECTO, CrearProyecto},
+        {CREAR_USUARIO, CrearUsuario},
+        {SALIR, Salir}
+    };
+
+    //bucle infinito de la maquina de estados
+    for(;;){
+        if(MachineState > STATE_NUM){
+            puts("Ocurrio un error");
+            exit(0);
+        }else{
+                (*Machine[MachineState].funcion)(&MachineState);
+        }
+    }
+
+    return 0;
+}
+
+//Menu
+void LogIn(States* State){
+    char usuario[50],contrasena[50];
+    puts("Introduzca su nombre de usuario: ");
+    scanf("%s", usuario);
+    puts("Introduzca su contraseña: ");
+    scanf("%s", contrasena);
+    *State=MENU_PRINCIPAL;
+}
+
+void MenuPrincipal(States *State){
+  system("clear");
+  int Opcion;
+  puts("1. Ver proyectos");
+  puts("2. Crear proyectos");
+  puts("3. Crear usuario");
+  puts("4. Salir");
+  scanf("%d", &Opcion);
+
+    switch(Opcion){
+        case 1:
+            *State = VER_PROYECTOS;
+            break;
+        case 2:
+            *State = CREAR_PROYECTO;
+            break;
+        case 3:
+            *State = CREAR_USUARIO;
+            break;
+        case 4:
+            *State = SALIR;
+            break;
+        default:
+            puts("Opcion invalida");
+    }
+}
+
+void VerProyectos(States *State){
+  system("clear");
+  int Opcion;
+  puts("Los proyectos disponibles son los siguientes, seleccione alguno: ");
+  puts("(Se selecciona alguno)");
+
+  puts("1. Commit");
+  puts("2. Revert");
+  puts("3. Pull");
+  puts("4. Status");
+  puts("5. Regresar");
+  scanf("%d", &Opcion);
+
+    switch(Opcion){
+        case 1:
+            *State = COMMIT;
+            break;
+        case 2:
+            *State = REVERT;
+            break;
+        case 3:
+            *State = PULL;
+            break;
+        case 4:
+            *State = STATUS;
+            break;
+        case 5:
+            *State = MENU_PRINCIPAL;
+            break;
+        default:
+            puts("Opcion invalida");
+    }
+}
+
+void Commit(States *State){
+  *State = MENU_PRINCIPAL;
+}
+
+void Revert(States *State){
+  *State = MENU_PRINCIPAL;
+}
+
+void Pull(States *State){
+  *State = MENU_PRINCIPAL;
+}
+
+void Status(States *State){
+  *State = MENU_PRINCIPAL;
+}
+
+void CrearProyecto(States *State){
+  *State=MENU_PRINCIPAL;
+}
+
+void CrearUsuario(States *State){
+  *State=MENU_PRINCIPAL;
+}
+
+void Salir(States *State){
+  exit(0);
+}
