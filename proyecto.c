ab9210d17e2f1651c992b1f6964846d242ac4cde
halabito8 <halabito8@gmail.com>
2019-12-03 09:24:40 -0600

    hola

--- proyecto.c
+++ proyecto.c
@@ -1,7 +1,10 @@
+
+
 // Inclusion de bibliotecas
 #include "maquinas_de_estado.h"
 #include<stdio.h>
 #include<string.h>
+#include<time.h>
 
 typedef struct def_Usuarios{
   char usuario[20], contra[20];
@@ -20,15 +23,18 @@ typedef struct def_MasterBranch{
 
 typedef struct def_Commit{
   int numcom,pull,lineas[100];
-  char ncommit[100], fecha[20], usuario[20], nproyecto[20];
+  char ncommit[100], fecha[30], usuario[20], nproyecto[20];
   struct def_Commit *sig;
 }tipocommit;
 
+//Variables globales
+//Inicios de las listas dinamicas, ademas de variables de contral de usuario y proyecto
 tipousuarios *iniciousr=NULL;
 tipocommit *iniciocom=NULL;
 tipomasterb *iniciomb=NULL;
 tipouproyecto *iniciousrpro=NULL;
 char usractual [20];
+char proactual[20];
 
 int main(void){
     system("clear");
@@ -69,7 +75,6 @@ void LeerArch(States* State){
   tipomasterb *temp5, *temp6;
   tipocommit *temp7, *temp8;
   FILE *archivo;
-
   //Se leen todos los USUARIOS
   archivo=fopen("usuarios.txt","rt");
   if(archivo!=NULL){
@@ -80,6 +85,7 @@ void LeerArch(States* State){
       fgets(string,19,archivo)!=NULL;
       strcpy(temp->contra,string);
       temp->contra[strlen(temp->contra)-1]='\0';
+      temp->sig=NULL;
       if(iniciousr!=NULL){
         temp2= iniciousr;
         while(temp2->sig!=NULL)
@@ -105,6 +111,7 @@ void LeerArch(States* State){
 	fgets(string,19,archivo)!=NULL;
 	strcpy(temp3->nproyecto,string);
 	temp3->nproyecto[strlen(temp3->nproyecto)-1]='\0';
+  temp3->sig=NULL;
 	if(iniciousrpro!=NULL){
 	  temp4=iniciousrpro;
 	  while(temp4->sig!=NULL)
@@ -129,6 +136,7 @@ void LeerArch(States* State){
 	fgets(string,19,archivo)!=NULL;
 	strcpy(temp5->nproyecto,string);
 	temp5->nproyecto[strlen(temp5->nproyecto)-1]='\0';
+  temp5->sig=NULL;
 	if(iniciomb!=NULL){
 	  temp6=iniciomb;
 	  while(temp6->sig!=NULL)
@@ -154,7 +162,7 @@ void LeerArch(States* State){
 	strcpy(temp7->nproyecto,string);
 	temp7->nproyecto[strlen(temp7->nproyecto)-1]='\0';
 
-	fgets(string,19,archivo);
+	fgets(string,29,archivo);
 	strcpy(temp7->fecha,string);
 	temp7->fecha[strlen(temp7->fecha)-1]='\0';
 
@@ -163,11 +171,12 @@ void LeerArch(States* State){
 	temp7->usuario[strlen(temp7->usuario)-1]='\0';
 
 	fscanf(archivo, "%i\n", &temp7->numcom);
-
+  temp7->sig=NULL;
+  
 	if(iniciocom!=NULL){
 	  temp8=iniciocom;
 	  while(temp8->sig!=NULL)
-	    temp8=temp7->sig;
+	    temp8=temp8->sig;
 	  temp8->sig=temp7;
 	}
 	else
@@ -175,6 +184,7 @@ void LeerArch(States* State){
       }
       fclose(archivo);
     }
+    puts("Que haces aqui?");
   *State=LOG_IN;
 }
 
@@ -231,16 +241,37 @@ void MenuPrincipal(States *State){
 }
 
 void VerProyectos(States *State){
-  int Opcion;
+  int Opcion,i=0;
+  tipouproyecto *busca;
+  char proyectos[30][20];
   puts("Los proyectos disponibles son los siguientes, seleccione alguno: ");
-  puts("(Se selecciona alguno)");
+  busca=iniciousrpro;
+  while(busca!=NULL){
+    if(strcmp(busca->usuario,usractual)==0){
+      strcpy(proyectos[i],busca->nproyecto);
+      i++;
+      printf("%i.- ",i);
+      puts(busca->nproyecto);
+    }
+    busca=busca->sig;
+  }
+  if(i==0){
+    system("clear");
+    puts("No tienes proyectos");
+    getchar();
+    *State=MENU_PRINCIPAL;
+  }
+  else{
+    __fpurge(stdin);
+    scanf(" %i",&Opcion);
+    strcpy(proactual,proyectos[Opcion-1]);
 
-  puts("1. Commit");
-  puts("2. Revert");
-  puts("3. Pull");
-  puts("4. Status");
-  puts("5. Regresar menu anterior");
-  scanf("%d", &Opcion);
+    puts("1. Commit");
+    puts("2. Revert");
+    puts("3. Pull");
+    puts("4. Status");
+    puts("5. Regresar menu anterior");
+    scanf("%d", &Opcion);
 
     switch(Opcion){
         case 1:
@@ -261,10 +292,103 @@ void VerProyectos(States *State){
         default:
             puts("Opcion invalida");
     }
+  }
 }
 
 void Commit(States *State){
-  *State = MENU_PRINCIPAL;
+  char string[1000], string2[1000], archcommit[50], archcommit2[100],buffer[200], flag[4];
+  tipocommit *busca,*comactual=NULL,*nuevo;
+  int numeroc=0, pull, pulleable;
+  FILE *archivo, *archivo2;
+
+  //uso de la libreria time.h
+  time_t now = time(NULL);
+  time(&now);
+  struct tm *mytime = localtime(&now);
+  //now=time(NULL);
+
+  mytime=localtime(&now);
+  sprintf(buffer, "%d:%d-%d-%d-%d_", mytime->tm_hour, mytime->tm_min,mytime->tm_mday, mytime->tm_mon+1, mytime->tm_year+1900);
+
+  busca=iniciocom;
+  while(busca!=NULL){
+    if(strcmp(busca->usuario, usractual)==0 && strcmp(busca->nproyecto, proactual)==0)
+      comactual=busca;
+    busca=busca->sig;
+  }
+  puts("Escribe el nombre de archivo que deseas hacer commit");
+  __fpurge(stdin);
+  gets(archcommit);
+  printf("adios\n");
+  if(comactual!=NULL){
+    printf("HOLA\n");
+    numeroc=comactual->numcom+1;
+  }
+  else{
+    printf("hola\n");
+    puts("Todavia no tiene ningun commit anterior");
+    numeroc=1;
+  }
+  archivo= fopen(archcommit, "rt");
+  if(archivo!=NULL){
+    strcpy(archcommit2,proactual);
+    strcat(archcommit2, usractual);
+    strcat(archcommit2, buffer);
+    sprintf(flag,"%d", numeroc);
+    strcat(archcommit2, flag);
+    puts(archcommit2);
+    strcat(archcommit2, ".txt");
+
+    archivo2=fopen(archcommit2, "wt");
+
+    while(fgets(string, 999, archivo)!=NULL){
+      string[strlen(string)-1]='\0';
+      fprintf(archivo2, "%s\n", string);
+    }
+    fclose(archivo);
+    fclose(archivo2);
+    puts("Commit realizado existosamente!");
+    puts("Este archivo es pulleable?");
+    printf("1. Si\n2. No\n");
+    __fpurge(stdin);
+    scanf("%d", &pulleable);
+    if(pulleable==1){
+      pull=1;
+    }
+    else{
+      pull=0;
+    }
+    printf("Valor de pull: %d\n", pull);
+    getchar();
+    nuevo=(tipocommit*)malloc(sizeof(tipocommit));
+    nuevo->numcom=numeroc;
+    nuevo->pull=pull;
+    strcpy(nuevo->ncommit,archcommit2);
+    strcpy(nuevo->fecha,buffer);
+    strcpy(nuevo->usuario,usractual);
+    strcpy(nuevo->nproyecto,proactual);
+    nuevo->sig=NULL;
+    if(iniciocom!=NULL){
+    busca=iniciocom;
+      while(busca->sig!=NULL)
+        busca=busca->sig;
+      busca->sig=nuevo;
+    }
+    else
+      iniciocom=nuevo;
+    *State=MENU_PRINCIPAL;
+
+  }
+  else{
+  puts("El archivo insertado no existe");
+  getchar();
+  system("clear");
+  *State=VER_PROYECTOS;
+}
+
+
+
+
 }
 
 void Revert(States *State){
@@ -303,48 +427,44 @@ void CrearProyecto(States *State){
     strcpy(nuevousuario->usuario,usractual);
     strcpy(nuevousuario->nproyecto,nombre);
     nuevousuario->sig=NULL;
-    if(iniciousrpro!=NULL)
-      {
-	busca3=iniciousrpro;
-	while(busca3->sig!=NULL)
-	  busca3=busca3->sig;
-	busca3->sig=nuevousuario;
+    if(iniciousrpro!=NULL){
+    	busca3=iniciousrpro;
+    	while(busca3->sig!=NULL)
+    	  busca3=busca3->sig;
+    	busca3->sig=nuevousuario;
       }
     else
       iniciousrpro=nuevousuario;
-    do
-      {
-	printf("Inserta el nombre del usuario %d:\n", i+1);
-	__fpurge(stdin);
-	gets(nombreusu);
-	busca2=iniciousr;
-	while(busca2!=NULL && strcmp(busca2->usuario,nombreusu)!=0)
-	  busca2=busca2->sig;
-	if(busca2!=NULL)
-	    {
-	    i++;
-	    nuevousuario=(tipouproyecto*)malloc(sizeof(tipouproyecto));
-	    strcpy(nuevousuario->usuario,nombreusu);
-	    strcpy(nuevousuario->nproyecto,nombre);
-	    nuevousuario->sig=NULL;
-	    if(iniciousrpro!=NULL)
-	      {
-		busca3=iniciousrpro;
-		while(busca3->sig!=NULL)
-		  busca3=busca3->sig;
-		busca3->sig=nuevousuario;
+    do{
+    	printf("Inserta el nombre del usuario %d:\n", i+1);
+    	__fpurge(stdin);
+    	gets(nombreusu);
+    	busca2=iniciousr;
+    	while(busca2!=NULL && strcmp(busca2->usuario,nombreusu)!=0)
+    	  busca2=busca2->sig;
+    	if(busca2!=NULL){
+  	    i++;
+  	    nuevousuario=(tipouproyecto*)malloc(sizeof(tipouproyecto));
+  	    strcpy(nuevousuario->usuario,nombreusu);
+  	    strcpy(nuevousuario->nproyecto,nombre);
+  	    nuevousuario->sig=NULL;
+  	    if(iniciousrpro!=NULL){
+      		busca3=iniciousrpro;
+      		while(busca3->sig!=NULL)
+      		  busca3=busca3->sig;
+      		busca3->sig=nuevousuario;
 	      }
-	    else
-	      iniciousrpro=nuevousuario;
-	  }
-	else
-	  {
-	  puts("Ese usuario no existe, inserte uno que este dado de alta");
-	  }
-      }while(i<Nusuarios);
+  	    else
+  	      iniciousrpro=nuevousuario;
+  	  }
+  	  else
+  	  {
+  	  puts("Ese usuario no existe, inserte uno que este dado de alta");
+  	  }
+    }while(i<Nusuarios);
 
-      }
-    *State=MENU_PRINCIPAL;
+  }
+  *State=MENU_PRINCIPAL;
 
 }
 
